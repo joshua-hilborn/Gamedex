5cf14db3077f601a31230c422ea9a9cbbd8c1284
joshua-hilborn <josh.hilborn@gmail.com>
2019-11-14 08:41:11 -0800

    style: cleaned up gamedatabase

--- app/src/main/java/com/vi/gamedex/database/GameDatabase.java
+++ app/src/main/java/com/vi/gamedex/database/GameDatabase.java
@@ -40,36 +40,3 @@ public abstract class GameDatabase extends RoomDatabase {
     public abstract GameDao gameDao();
 
 }
-
-
-
-
-/*
-@Database(entities = {MovieFavorite.class}, version = 1, exportSchema = false)
-public abstract class MovieDatabase extends RoomDatabase {
-
-    private static final String LOG_TAG = MovieDatabase.class.getSimpleName();
-    private static final Object LOCK = new Object();
-    private static final String DATABASE_NAME = "favoriteMovies";
-    private static MovieDatabase sInstance;
-
-    public static MovieDatabase getInstance( Context context ){
-        if (sInstance == null){
-            synchronized (LOCK){
-                Log.d(LOG_TAG, "getInstance: Creating new database instance");
-                sInstance = Room.databaseBuilder(
-                        context.getApplicationContext(),
-                        MovieDatabase.class,
-                        MovieDatabase.DATABASE_NAME)
-                        .build();
-            }
-        }
-        Log.d(LOG_TAG, "getInstance: Getting database instance");
-        return sInstance;
-    }
-
-    public abstract MovieDao movieDao();
-
-}
-
- */
