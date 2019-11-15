fe80394b651223fb32771d2b933377707fc7dae2
joshua-hilborn <josh.hilborn@gmail.com>
2019-11-14 21:10:31 -0800

    feat: Admob banner ad implemented
    using test ad package and app id

--- app/src/main/java/com/vi/gamedex/ui/MainActivity.java
+++ app/src/main/java/com/vi/gamedex/ui/MainActivity.java
@@ -6,6 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.material.tabs.TabLayout;
 import com.vi.gamedex.R;
 import com.vi.gamedex.adapter.PageAdapter;
@@ -16,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     TabLayout tabLayout;
     ViewPager viewPager;
     PageAdapter pageAdapter;
+    AdView adView;
 
 
     @Override
@@ -33,6 +39,27 @@ public class MainActivity extends AppCompatActivity {
         // Stop Tabs from refreshing by navigating to different tab, cache them
         viewPager.setOffscreenPageLimit(tabLayout.getTabCount());
         tabLayout.setupWithViewPager(viewPager);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+
+            }
+        });
+        adView = findViewById(R.id.av_main);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+
+        /*
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+         */
 
     }
 
